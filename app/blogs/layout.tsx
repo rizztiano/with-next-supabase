f6ec0b6c59870e0eb854ccee699e7dbe21169401
rizztiano <emmanuel@syntacticsinc.com>
2026-01-12 16:57:07 +0800

    init

---
+++ app/blogs/layout.tsx
@@ -0,0 +1,30 @@
+import { AuthButton } from '@/components/auth-button'
+import { EnvVarWarning } from '@/components/env-var-warning'
+import { ThemeSwitcher } from '@/components/theme-switcher'
+import { hasEnvVars } from '@/lib/utils'
+import { Suspense } from 'react'
+
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <main className="min-h-screen flex flex-col items-center">
+         <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                  <div className="flex gap-2 items-center font-semibold">
+                     <p>Blog App</p>
+                     <ThemeSwitcher />
+                  </div>
+                  {!hasEnvVars ? (
+                     <EnvVarWarning />
+                  ) : (
+                     <Suspense>
+                        <AuthButton />
+                     </Suspense>
+                  )}
+               </div>
+            </nav>
+            <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+         </div>
+      </main>
+   )
+}
---
+++ app/blogs/page.tsx
@@ -0,0 +1,3 @@
+export default async function ProtectedPage() {
+   return <div className="flex-1 w-full flex flex-col gap-12">helloworld</div>
+}
