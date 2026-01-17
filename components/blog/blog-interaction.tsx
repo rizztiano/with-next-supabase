94616cd72000ee07f8cc606f0606d5ff7deb2239
rizztiano <emmanuel@syntacticsinc.com>
2026-01-17 13:02:25 +0800

    feat: comment section

--- components/blog/blog-interaction.tsx
+++ components/blog/blog-interaction.tsx
@@ -1,15 +1,26 @@
 import { AutosizeTextarea } from '@/components/ui/autosize-textarea'
 import { Button } from '@/components/ui/button'
+import { CircleArrowRight, Link } from 'lucide-react'
 
 const BlogInteraction = () => {
    return (
       <div className='flex flex-col w-full gap-6'>
-         <div className='flex justify-between'>
+         <div className='flex items-center justify-between'>
             <h3 className='text-xl font-medium'>Comments (1)</h3>
             <Button className='bg-green-600 hover:bg-green-700'>Add comment</Button>
          </div>
          <div className='flex flex-col'>
-            <AutosizeTextarea className='resize-none' minHeight={0} />
+            <div className='flex flex-col gap-3'>
+               <AutosizeTextarea className='resize-none text-base' minHeight={0} />
+               <div className='flex justify-end w-full gap-2'>
+                  <div className='flex justify-end w-full items-center gap-1'>
+                     <div className='flex gap-2'>
+                        <Link size={20} className='text-orange-600' />
+                     </div>
+                     <CircleArrowRight size={24} className='text-blue-500 cursor-pointer' />
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
    )
